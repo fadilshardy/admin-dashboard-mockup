e8d105ac640d38401d90ca525cd551b7c4691940
fadilshardy <fadilshrdy@gmail.com>
2022-05-27 00:05:35 +0700

    initial commit with layout scaffolding

---
+++ public/script.js
@@ -0,0 +1,6 @@
+// const divs = document.querySelectorAll('div');
+
+// divs.forEach(div => {
+//     div.style.backgroundColor = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
+//     div.style.borderColor = '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6);
+// });
